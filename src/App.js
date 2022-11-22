3a4b08ea5f5b603c334edf77abc762fb45c09dc8
Brandddd <b.palacio1@utp.edu.co>
2022-11-22 01:16:02 -0500

    Añadiendo rutas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom"; // Dos componentes, Routes para definir multiples rutas, y Route para definir una sola ruta
+import { Home } from "./components/Home";
+import { Login } from "./components/Login";
+import { Register } from "./components/Register";
+import { AuthProvider } from "./context/Authentication";  // Importación del componente que se creó, AuthProvider
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </AuthProvider>
   );
 }
 
-export default App;
+// Note 1: Se importa Route y Routes desde react-router-dom
+// Se llama <Routes> dentro de function App de tal forma que nos retorne las rutas
+// Se le asignan los Path, primer caso: /home element: Home, y así sucesivamente.
